0bb5cd10fa01660aa2ea1e7c48e13bc54d28250f
c12m07 <ximic4@gmail.com>
2022-10-17 18:10:51 -0500

    Solucion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'wouter';
+import Home from './components/Home';
+import Form from './components/form/index';
+import Registered from './components/registered/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/form'>
+          <Form />
+        </Route>
+
+        <Route path='/registered'>
+          <Registered />
+        </Route>
+
+        <Route>
+          <div className='ErrorPage'>
+            404 Not found
+          </div> 
+        </Route>
+
+      </Switch>
     </div>
   );
 }
